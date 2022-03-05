26d3f0b28b9064ccd5d185804e21193c329dd431
Oleg <viktorovich959595@mail.ru>
2022-03-05 18:41:55 +1000

    Updates

---
+++ precache-manifest.7843095889a53416df09243ea677415e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac4895b14e76d0e927fe77c8ba53029",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "275b74a49f6293df2396",
+    "url": "/simple-react-homeworks/static/css/main.ff0c23af.chunk.css"
+  },
+  {
+    "revision": "a4f981484b22353e3443",
+    "url": "/simple-react-homeworks/static/js/2.bc8df3c9.chunk.js"
+  },
+  {
+    "revision": "7c3782a7e65f48d3b45b3ebb61610f23",
+    "url": "/simple-react-homeworks/static/js/2.bc8df3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275b74a49f6293df2396",
+    "url": "/simple-react-homeworks/static/js/main.86c867ba.chunk.js"
+  },
+  {
+    "revision": "35a9557543d5cb14c0c5",
+    "url": "/simple-react-homeworks/static/js/runtime-main.5a451111.js"
+  }
+]);
