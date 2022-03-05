ae3300c65b913128f53e5f1d2c7552b83670a594
Oleg <viktorovich959595@mail.ru>
2022-03-05 19:05:42 +1000

    final

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import s from './App.module.css'
 import {HW5} from "../../../p2-homeworks/h5/HW5";
-import HW1 from "../../../p2-homeworks/h1/HW1";
 
 export function App() {
     return (
         <div className={s.App}>
             <div>react homeworks:</div>
-            <HW1/>
+            <HW5/>
         </div>
     )
 }
